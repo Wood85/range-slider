6aa06da286cb1050b36b49497104c2b9d16e6851
Alexandr <megalistnn@gmail.com>
2023-01-15 21:48:06 +0300

    installed webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/index.scss';
+
+const rangeSliderData = {
+  size: 'big'
+};
+
+const rangeSlider = {
+  title: 'range-slider',
+  ...rangeSliderData
+};
+
+console.log(rangeSlider);
