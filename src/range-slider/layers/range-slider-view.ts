e30cb90d8b9bde5b6f9a11031107deaeba8b057b
Alexandr <megalistnn@gmail.com>
2023-05-01 08:40:51 +0300

    start writing css

--- src/range-slider/layers/range-slider-view.ts
+++ src/range-slider/layers/range-slider-view.ts
@@ -8,11 +8,11 @@ export class RangeSliderView {
   createElem(tag: string, props: Props) {
     let element: any;
     element = document.createElement(tag);
-    element.classList.add(props.class);
-    element.type = props.type;
-    element.setAttribute('value', props.value);
-    element.setAttribute('min', props.min);
-    element.setAttribute('max', props.max);
+    if(props.class) element.classList.add(props.class);
+    if(props.type)element.type = props.type;
+    if(props.value)element.setAttribute('value', props.value);
+    if(props.min)element.setAttribute('min', props.min);
+    if(props.max)element.setAttribute('max', props.max);
 
     return element
   }
@@ -20,37 +20,43 @@ export class RangeSliderView {
   createRangeSlider() {
 
       if(this.container !== null){
-          const values: HTMLElement = this.createElem('div', {class: 'range-slider__values'});
-          const slider: HTMLElement = this.createElem('div', {class: 'range-slider__slider'});
-          const range: HTMLElement = this.createElem('div', {class: 'range-slider__range'});
+          const values: HTMLElement = this.createElem('div', {class: 'values'});
+          const slider: HTMLElement = this.createElem('div', {class: 'slider'});
+          const range: HTMLElement = this.createElem('div', {class: 'range'});
           const inputMin: HTMLInputElement = this.createElem('input', {
-            class: 'value__input_min',
+            class: 'values__input',
             type: 'number',
             value: '2500'
           });
+          inputMin.classList.add('values__input_min');
           const inputMax: HTMLInputElement = this.createElem('input', {
-            class: 'value__input_max',
+            class: 'values__input',
             type: 'number',
             value: '7500'
           });
+          inputMax.classList.add('values__input_max');
+          const separator: HTMLElement = this.createElem('div',{class: 'values__separator'});
+          separator.textContent = '-';
           const progress: HTMLElement = this.createElem('div', {class: 'slider__progress'});
           const leftRunner: HTMLInputElement = this.createElem('input', {
-              class: 'range__runner_left',
+              class: 'range__runner',
               type: 'range',
               value: '2500',
               min:'0',
               max: '1000'
           });
+          leftRunner.classList.add('range__runner_left');
           const rightRunner: HTMLInputElement = this.createElem('input', {
-              class: 'range__runner_right',
+              class: 'range__runner',
               type: 'range',
               value: '7500',
               min:'0',
               max: '1000'
           });
+          rightRunner.classList.add('range__runner_right');
 
 
-          values.append(inputMin, inputMax);
+          values.append(inputMin, separator, inputMax);
           slider.append(progress);
           range.append(leftRunner, rightRunner)
           this.container.append(values, slider, range);
